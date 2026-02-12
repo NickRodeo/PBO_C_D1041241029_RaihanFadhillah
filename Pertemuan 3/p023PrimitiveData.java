e78d1a6f4e17d93caee9ef9f087b108530df8295
NickRodeo <rodeonick708@gmail.com>
2026-02-12 16:48:39 +0700

    Pertemuan 3 Primitive Data

---
+++ Pertemuan 3/p023PrimitiveData.java
@@ -0,0 +1,33 @@
+/*
+Nim : D1041241029
+Nama : Raihan Fadhillah
+Program untuk tipe data primitive
+*/
+
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        // Tipe data bilangan bulat
+        int umur = 20;
+        Long populasi = 8000000000L;
+        
+        // Tipe data bilangan desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        
+        // Tipe data karakter
+        char nilaiHuruf = 'A';
+
+        // Tipe data logika
+        boolean lulus = true;
+
+        // Menampikan niai semua variabel
+        System.out.println("Umur            : " + umur);
+        System.out.println("Populasi        : " + populasi);
+        System.out.println("Tinggi Badan    : " + tinggi);
+        System.out.println("IPK             : " + ipk);
+        System.out.println("Nilai Huruf     : " + nilaiHuruf);
+        System.out.println("Status Lulus    : " + lulus);
+
+    }
+    
+}
