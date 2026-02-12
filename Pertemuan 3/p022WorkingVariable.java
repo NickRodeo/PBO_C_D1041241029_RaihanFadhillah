ee35009522efa2ecda1feaa2d4a574946ad245ec
NickRodeo <rodeonick708@gmail.com>
2026-02-12 16:38:04 +0700

    Pertemuan 3

---
+++ Pertemuan 3/p022WorkingVariable.java
@@ -0,0 +1,19 @@
+/*
+Nim : D1041241029
+Nama : Raihan Fadhillah
+Program untuk variable
+*/
+
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int angka = 10;
+        // Menampilkan nilai angka
+        System.out.println("Nilai awal angka: " + angka);
+        // Mengubah nilai variabel
+        angka = 25;
+        // Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+    
+}
