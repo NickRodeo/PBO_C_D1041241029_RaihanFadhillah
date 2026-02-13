2f0fa6f4e2d90fa1f44976d3c526ab8159a3ed14
NickRodeo <rodeonick708@gmail.com>
2026-02-13 16:43:44 +0700

    Pengerjaan contoh program

---
+++ Pertemuan 4/p041NamingConvention.java
@@ -0,0 +1,21 @@
+/*
+Nim : D1041241029
+Nama : Raihan Fadhillah
+Program untuk belajar terkait naming convention atau penamaan variabel agar lebih mudah dimengerti dan menghasilkan kode yang bagus
+*/
+
+public class p041NamingConvention {
+    // Constanta Variabel
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+        // Variabel dengan camelCase
+        int studentAge = 19;
+        String studentName = "Raihan Fadhillah";
+        boolean isActive = true;
+        System.out.println("Nama: " + studentName);
+        System.out.println("Umur: " + studentAge);
+        System.out.println("Status: " + isActive);
+        System.out.println("Konstanta PI: " + PI);
+}
+
+}
